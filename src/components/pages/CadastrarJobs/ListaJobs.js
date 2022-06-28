90b2ccedc5715fb3b87c843cd492e7fa1e186253
CarlosSousa2001 <carlosramos10k@gmail.com>
2022-06-28 17:02:08 -0300

    bugs corrigidos e com base css

--- src/components/pages/CadastrarJobs/ListaJobs.js
+++ src/components/pages/CadastrarJobs/ListaJobs.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-
 export default class ListaJobs extends Component {
  state = {
     verJobs: []
  }
-
  componentDidMount() {
     this.mostrarJobs()
   }
-
    mostrarJobs = () => {
+   
         axios.get('https://labeninjas.herokuapp.com/jobs', {
           headers: {
             Authorization: "30167c40-cd85-49b4-96f6-7d20b939127f"
@@ -20,21 +18,36 @@ export default class ListaJobs extends Component {
         }).catch ((err) => {
           console.log(err.message)
         })
- 
     }
- 
- 
+    deletarJobs = (id) => {
+      if(window.confirm("Deseja deletar") ===  true) {
+      const url = `https://labeninjas.herokuapp.com/jobs/${id}`
+      axios.delete(url, {
+        headers:{
+          Authorization: "30167c40-cd85-49b4-96f6-7d20b939127f"
+        }
+      })
+      .then(()=>{
+       alert("Deletado")
+       this.mostrarJobs()
+      })
+      .catch((err)=>{
+       alert('Ocorreu um erro')
+      })
+    } else {
+
+    }
+    }
+
     render() {
         const listaJobs = this.state.verJobs.map((job) => {
             return (
               <div key={job.id}>
                 {job.title}
+                <button onClick={()=>this.deletarJobs(job.id)}>Deletar</button>
               </div>
             )
           })
-
-
-
     return (
       <div>
         {listaJobs}
--- src/components/pages/Pagehome.js
+++ src/components/pages/Pagehome.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 
+const Head = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 800px;
+    margin: 0 auto;
+    height: 90px;
+    h1 {
+      color: #ccc;
+    }
+`
+const AreaP = styled.div`
+  display: flex;
+  p {
+    margin: 10px;
+    cursor: pointer;
+    :hover {
+      color: #5710B3;
+    }
+  }
+`
+
 export default class Pagehome extends Component {
   render() {
     return (
       <div>
-        <h1>labeninja</h1>
-        <button onClick={this.props.irParaHome}>Ir para home</button>
-        <button onClick={this.props.irParaCarrinho}>Ir para carrinho</button>
-        <hr/>
+      <header>
+        <Head>
+        <h1>Labeninja</h1>
+        <AreaP>
+        <p onClick={this.props.irParaHome}>Home</p>
+        <p onClick={this.props.irParaCarrinho}> Carrinho</p>
+        </AreaP>
+        </Head>
+        </header>
       </div>
     )
   }
